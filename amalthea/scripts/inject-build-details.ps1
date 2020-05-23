9d4f8601c73aa0e00bf207e6c7ec8e88f82a4436
Dominic Maas <dominic.maas@live.com>
2020-05-24 11:35:50 +1200

    Add assets from other repo

---
+++ amalthea/scripts/inject-build-details.ps1
@@ -0,0 +1,7 @@
+﻿$date = Get-Date -Format g
+$branch = $Env:BUILD_SOURCEBRANCH
+$commit = $Env:BUILD_SOURCEVERSION
+
+'{"branch": "' + $branch + '","commit":"' + $commit + '","platform":"UWP","time":"' + $date + '"}' | Out-File 'amalthea/src/apps/uwp/build_info.json'
+'{"branch": "' + $branch + '","commit":"' + $commit + '","platform":"iOS","time":"' + $date + '"}' | Out-File 'amalthea/src/apps/ios/Resources/build_info.json'
+'{"branch": "' + $branch + '","commit":"' + $commit + '","platform":"Android","time":"' + $date + '"}' | Out-File 'amalthea/src/apps/android/Assets/build_info.json'
