89febf2e9d3ebd44b375c234f403464b4cf24500
Dominic Maas <dominic.maas@live.com>
2020-05-23 12:52:10 +1200

    Update InjectJsonInfo.ps1

--- legacy/scripts/InjectJsonInfo.ps1
+++ legacy/scripts/InjectJsonInfo.ps1
@@ -1,3 +1,3 @@
 ﻿$date = Get-Date -Format g
 
-'{"build_branch":"master","build_no":"'+$Env:BUILD_BUILDNUMBER+'","build_time":"'+$date+'"}' | Out-File 'legacy/uwp/Assets/build_info.json'
+'{"build_mood":"master","build_no":"'+$Env:BUILD_BUILDNUMBER+'","build_time":"'+$date+'"}' | Out-File 'legacy/src/uwp/Assets/build_info.json'
